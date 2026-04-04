fdf798135c996b2537ef1cfd9b249a7fb00a347f
wkatir <wilmerhenrysalazarmartinez@gmail.com>
2026-04-04 15:57:24 -0600

    Update app colors to match brand (#071019, #F28A14, #EF8A14, #F08B17)

--- frontend/lib/core/theme/app_colors.dart
+++ frontend/lib/core/theme/app_colors.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static const accent = Color(0xFFF18A17);
-  static const accentLight = Color(0xFFF5A94E);
-  static const accentDark = Color(0xFFEE8B16);
+  static const accent = Color(0xFFF28A14);
+  static const accentLight = Color(0xFFF08B17);
+  static const accentDark = Color(0xFFEF8A14);
 
-  static const background = Color(0xFF08111A);
-  static const surface = Color(0xFF071019);
-  static const surfaceElevated = Color(0xFF0E1926);
+  static const background = Color(0xFF071019);
+  static const surface = Color(0xFF081521);
+  static const surfaceElevated = Color(0xFF0D1A2A);
 
   static const borderSubtle = Color(0xFF162234);
   static const borderStrong = Color(0xFF253650);
@@ -18,6 +18,6 @@ abstract class AppColors {
 
   static const success = Color(0xFF4ADE80);
   static const danger = Color(0xFFF87171);
-  static const warning = Color(0xFFF18A17);
+  static const warning = Color(0xFFF28A14);
   static const info = Color(0xFF60A5FA);
 }
